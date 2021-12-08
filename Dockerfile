08dd47beafb6aa3c873a1293c0fd1fbde75986e2
iorn <wmt.tkn.121@gmail.com>
2021-12-08 13:33:26 +0900

    repair function of converting character format

--- Dockerfile
+++ Dockerfile
@@ -1,61 +1,22 @@
-# FROM python:3
-# USER root
+FROM python:3
+USER root
 
-# RUN apt-get update
-# RUN apt-get -y install locales && \
-#     localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
-# ENV LANG ja_JP.UTF-8
-# ENV LANGUAGE ja_JP:ja
-# ENV LC_ALL ja_JP.UTF-8
-# ENV TZ JST-9
-# ENV TERM xterm
+RUN apt-get update
+RUN apt-get -y install locales && \
+    localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+ENV LANG ja_JP.UTF-8
+ENV LANGUAGE ja_JP:ja
+ENV LC_ALL ja_JP.UTF-8
+ENV TZ JST-9
+ENV TERM xterm
 
-# RUN mkdir -p /root/src
-# RUN mkdir -p /root/data
-# RUN mkdir -p /root/output
-# COPY requirements.txt /root/src
-# COPY data/ /root/data
-# WORKDIR /root/src
+RUN mkdir -p /root/src
+RUN mkdir -p /root/data
+RUN mkdir -p /root/output
+COPY requirements.txt /root/src
+COPY data/ /root/data
+WORKDIR /root/src
 
-# RUN pip install --upgrade pip
-# RUN pip install --upgrade setuptools
-# RUN pip install -r requirements.txt
-
-########## Pull ##########
-FROM nvidia/cuda:8.0-cudnn7-devel-ubuntu16.04
-########## BASIS ##########
-RUN apt-get update && apt-get install -y \
-    vim \
-    wget \
-    unzip \
-    git \
-    build-essential
-########## PyTorch ##########
-RUN apt-get update && \
-    apt-get install -y \
-    python3-pip && \
-    pip3 install --upgrade "pip < 21.0" && \
-    pip3 install \
-    torch==1.0.0 \
-    torchvision==0.2.1
-########## Book "pytorch_advanced" ##########
-RUN cd /home && \
-    git clone https://github.com/YutaroOgawa/pytorch_advanced
-########## Jupyter Notebook ##########
-RUN pip3 install jupyter && \
-    echo "#!/bin/bash \n \
-    cd /home/pytorch_advanced && \n \ 
-    jupyter notebook --port 8000 --ip=0.0.0.0 --allow-root" \
-    >> /home/jupyter_notebook.sh && \
-    chmod +x /home/jupyter_notebook.sh
-########## Requirements ##########
-RUN apt-get update && \
-    apt-get install -y \
-    libopencv-dev && \
-    pip3 install \
-    matplotlib \
-    tqdm \
-    opencv-python \
-    pandas
-######### Initial position ##########
-WORKDIR /home
+RUN pip install --upgrade pip
+RUN pip install --upgrade setuptools
+RUN pip install -r requirements.txt
--- src/markov_generator.py
+++ src/markov_generator.py
@@ -47,42 +47,59 @@ def text_cleansing(text):
     return splitted_text
 
 
-# データの保管先
-ROOT_PATH_RAW = "/root/data/raw"
-ROOT_PATH_UTF = "/root/data/utf"
-
-
-def convert_charcode(raw_path, utf_path):
-    """[RAWのテキストファイルからUTF-8形式にエンコードして返す]
+def convert_src_folder(src_path, dest_path):
+    """[srcフォルダ内のテキストファイルをutf-8形式にして、別フォルダにコピー及び変換して追加する]
 
     Args:
-        raw_path ([string]): [description]
-        utf_path ([string]): [description]
+        src_path ([string]): [shift-jis形式のテキストファイルを格納したフォルダ]
+        dest_path ([string]): [utf-8形式のテキストファイルを格納するフォルダ]
 
     Returns:
-        [type]: [description]
+        [list(string)]: [変換したファイルリストを返す]
     """
+    src_files = os.listdir(src_path)
+    dest_files = os.listdir(dest_path)
+
+    for file_name in src_files:
+        if file_name in dest_files:
+            continue
+        src_file_path = src_path + file_name
+        dest_file_path = dest_path + file_name
+        # ファイルオブジェクトをそれぞれ開く
+        try:
+            with open(src_file_path, mode="r", encoding="shift_jis") as src, open(dest_file_path, mode="w", encoding="utf_8") as dest:
+                dest.write(src.read())
+        except:
+            return False
+    return True
+
+
+def main():
+    # データの保管先
+    ROOT_PATH_SRC = "/root/data/src/"
+    ROOT_PATH_DEST = "/root/data/dest/"
     splitted_text = ""
-    raw_files = os.listdir(raw_path)
-    utf_files = os.listdir(utf_path)
-    for file in raw_files:
-        file_path = raw_path + file
-        with open(file_path, mode="rb") as f:
-            chardet.detect()
+
+    if not convert_src_folder(ROOT_PATH_SRC, ROOT_PATH_DEST):
+        exit()
+
+    files = os.listdir(ROOT_PATH_DEST)
+    for text_file in files:
+        text_file_name = ROOT_PATH_DEST+text_file
+        with open(text_file_name, "r", encoding="utf-8") as f:
             text = f.read()
-        # fileだったら処理
-        splitted_text += text_cleansing(text)+"\n"
-    return splitted_text
+            splitted_text += text_cleansing(text)
+            splitted_text += "\n"
+    sentence = None
+    while sentence == None:
+        # モデルを生成
+        text_model = markovify.NewlineText(splitted_text, state_size=2)
+        # モデルから文章を生成
+        sentence = text_model.make_sentence(tries=100, min_words=100)
+
+    with open("../output/markov_sentence.txt", mode="a") as f:
+        f.write(sentence.replace(" ", ""))
+        f.write("\n\n")
 
 
-splitted_text = recursive_file_check(ROOT_PATH)
-sentence = None
-while sentence == None:
-    # モデルを生成
-    text_model = markovify.NewlineText(splitted_text, state_size=2)
-    # モデルから文章を生成
-    sentence = text_model.make_sentence(tries=100, min_words=100)
-
-with open("../output/markov_sentence.txt", mode="a") as f:
-    f.write(sentence.replace(" ", ""))
-    f.write("\n\n")
+main()
